f4120caf263e6facc07c76c7f4fcbfb456ce5e6f
Berz67 <danygg2021@gmail.com>
2025-10-30 21:12:37 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
 ID="3c58f86d-fe43-433c-8d91-e05c5f278393";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Berz67/SpawnerX-Hub/refs/heads/main/Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Berz67/SpawnerX-Hub/refs/heads/main/Loader.Lua"))()
